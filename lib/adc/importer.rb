7ebd0629cb73a24c696ba6a296b6df8b201567a6
Nick Stenning <nick@whiteink.com>
2009-12-30 22:37:55 +0000

    Add basic error checking to importer and create a PDF version of the imported file.

--- lib/adc/importer.rb
+++ lib/adc/importer.rb
@@ -23,20 +23,52 @@ module ADC
     def perform
       # This glob pattern should case-insensitively match *.tif or *.tiff files.
       Pathname.glob(@incoming_path + '*.[Tt][Ii][Ff]{[Ff],}').each do |f|
-        new_path = @archives_path + f.basename
-        f.rename(new_path)
-        create_attachment_for(new_path)
+        doc_id = f.basename.to_s[0...-f.extname.length] # "foo/bar/baz.tif" -> "baz"
+        create_folder_for(doc_id)
+        f.rename(original_path(doc_id)) # 'original' as in the original imported file.
+        create_attachment_for(doc_id)
+        create_pdf_for(doc_id)
       end
     end
     
-    def create_attachment_for(path)
-      Attachment.create(:url => path.relative_path_from(@archives_path).to_s)
+    protected
+    
+    def folder_path(doc_id)
+      @archives_path + doc_id
+    end
+    
+    def original_path(doc_id)
+      folder_path(doc_id) + "#{doc_id}.tif" # we standardise the original's extension.
+    end
+    
+    def pdf_path(doc_id)
+      folder_path(doc_id) + "#{doc_id}.pdf"
+    end
+    
+    def create_folder_for(doc_id)
+      # TODO: raise more enlightening error types
+      if folder_path(doc_id).exist?
+        raise Exception, "Can't import, an image with doc_id '#{doc_id}' already exists in '#{@archives_path}'"
+      else
+        folder_path(doc_id).mkdir
+      end
+    end
+    
+    def create_attachment_for(doc_id)
+      Attachment.create(:doc_id => doc_id)
+    end
+    
+    def create_pdf_for(doc_id)
+      unless Pathname(AppConfig.tiff2pdf).executable?
+        raise Exception, "tiff2pdf executable appears invalid: check '#{AppConfig.tiff2pdf}', as specified in config/archives.yml"
+      end
+      system("#{AppConfig.tiff2pdf} -o '#{pdf_path(doc_id)}' '#{original_path(doc_id)}'")
     end
     
     def check_paths!
       ['incoming', 'archives'].each do |name|
         path = instance_variable_get("@#{name}_path")
-        raise Errno::ENOENT, "#{name.capitalize} path '#{path}' does not exist!" unless path.exist?
+        raise Errno::ENOENT, "#{name.capitalize} path '#{path}' does not exist" unless path.exist?
       end
     end
     
--- spec/lib/adc/importer_spec.rb
+++ spec/lib/adc/importer_spec.rb
@@ -1,8 +1,29 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require 'tmpdir'
 require 'pathname'
+require 'base64'
+
+MINITIFF = 'SUkqAAoAAAAAABIAAAEDAAEAAAABAAAAAQEDAAEAAAABAAAAAgEDAAIAAAABAAEAAwEDAAEAAAAB' +
+           'AAAABgEDAAEAAAABAAAACgEDAAEAAAABAAAADQECAAkAAADoAAAAEQEEAAEAAAAIAAAAEgEDAAEA' +
+           'AAABAAAAFQEDAAEAAAACAAAAFgEDAAEAAAAAIAAAFwEEAAEAAAABAAAAGgEFAAEAAADyAAAAGwEF' +
+           'AAEAAAD6AAAAHAEDAAEAAAABAAAAKAEDAAEAAAABAAAAMQECADoAAAACAQAAUgEDAAEAAAACAAAA' +
+           'AAAAADF4MS50aWZmAAAAAABIAAAAAQAAAEgAAAABSW1hZ2VNYWdpY2sgNi40LjEgMDYvMTkvMDgg' +
+           'UTE2IGh0dHA6Ly93d3cuaW1hZ2VtYWdpY2sub3JnAA=='
+
+# TODO: Consider speed of PDF creation/TIFF writing. Is there any way we can 
+#       mock this stuff out?
 
 describe ADC::Importer do
+  
+  def create_tiff_file(pathname, name)
+    file = (pathname + name)
+    file.open('wb') do |f|
+      # Write out a 1x1 TIFF file from base64 encoded string.
+      f.write(Base64.decode64(MINITIFF))
+    end
+    return file
+  end
+  
   before do
     @tmpdir = Pathname(Dir.tmpdir + '/' + rand.to_s[2..8])
     @tmpdir.mkdir
@@ -12,8 +33,7 @@ describe ADC::Importer do
     
     [@incoming, @archives].each(&:mkdir)
     ['a', 'b', 'c'].each do |name|
-      file = @incoming + "#{name}.tif"
-      file.open('w')                          # Create the file.
+      file = create_tiff_file(@incoming, "#{name}.tif") # Create the file.
       instance_variable_set("@#{name}", file) # Set @a = Pathname(a.tif), etc.
     end
     
@@ -32,26 +52,27 @@ describe ADC::Importer do
   it "should raise an error if the incoming path doesn't exist" do
     lambda { 
       ADC::Importer.new('nonexistentincomingpath', @archives.to_s) 
-    }.should raise_error Errno::ENOENT, /Incoming path '.+' does not exist!/
+    }.should raise_error Errno::ENOENT, /Incoming path '.+' does not exist/
   end
   
   it "should raise an error if the archives path doesn't exist" do
     lambda { 
       ADC::Importer.new(@incoming.to_s, 'nonexistentarchivespath') 
-    }.should raise_error Errno::ENOENT, /Archives path '.+' does not exist!/
+    }.should raise_error Errno::ENOENT, /Archives path '.+' does not exist/
   end
   
-  it "should look for new TIF files in 'incoming' and move them to 'archives'" do
+  it "should look for new TIF files in 'incoming' and create a directory for their name in 'archives'" do
     @incoming.children.should == [@a, @b ,@c]
     @archives.children.should be_empty
     @i.perform
     @incoming.children.should be_empty
-    @archives.children.map(&:basename).should == [@a, @b, @c].map(&:basename)
+    @archives.children.map { |x| x.basename.to_s }.should == ['a', 'b', 'c']
+    @archives.children.first.should be_directory
   end
   
   it "should move TIF files with all common extensions" do
     ['d.tiff', 'e.TIF', 'f.TIFF'].each do |name|
-      (@incoming + name).open('w')
+      create_tiff_file(@incoming, name)
     end
     @archives.should have(0).children
     @i.perform
@@ -60,7 +81,7 @@ describe ADC::Importer do
   
   it "should not move non-TIF files" do
     ['d.jpg', 'e.TIF', 'f.txt'].each do |name|
-      (@incoming + name).open('w')
+      create_tiff_file(@incoming, name)
     end
     @incoming.should have(6).children
     @archives.should have(0).children
@@ -69,10 +90,21 @@ describe ADC::Importer do
     @archives.should have(4).children
   end
   
+  it "should raise an error when trying to import a file with a doc_id that matches an already-imported file" do
+    @i.perform
+    create_tiff_file(@incoming, 'a.TIFF')
+    lambda { @i.perform }.should raise_error Exception, /Can't import, an image with doc_id '.+' already exists/
+  end
+  
   it "should create Attachment entries in the database for each file it imports" do
     num_fixtures = 5 # This is the number of attachment fixtures.
     Attachment.all.length.should == num_fixtures 
     @i.perform
     Attachment.all.length.should == num_fixtures + 3
+  end
+  
+  it "should create a PDF version of the tif file with the same basename" do
+    @i.perform
+    (@archives + 'a' + 'a.pdf').should exist
   end
 end
