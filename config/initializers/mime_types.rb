b2a5940a8c3663d24c8e3b45a026f8e31a77f356
Nick Stenning <nick@whiteink.com>
2010-01-01 18:57:48 +0000

    Fix for const redefinition warnings.

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -4,4 +4,7 @@
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
 
-Mime::Type.register "application/pdf", :pdf
+# Avoid nasty 'already initialized constant' warnings
+unless Mime.const_defined?('PDF')
+  Mime::Type.register "application/pdf", :pdf
+end
